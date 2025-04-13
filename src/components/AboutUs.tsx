ee3748624180c0cd0b3492b97cbf589dde233f17
covey-codes <dustindark7@gmail.com>
2025-04-13 18:22:34 +0100

    desktop about Us section done

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -7,11 +7,36 @@ const AboutUs = () => {
         <h1 className="text-white bg-[#035772] p-5 rounded-[20px]">About Us</h1>
       </div>
 
-      <div className="flex justify-center">
+      <div className="flex justify-center mb-12">
         <InfoBox
           title="1. Who are we?"
           content="At Nexora, we are driven to help students achieve their dreams. By leveraging our expertise in student recruitment, we connect ambitious individuals with top universities and colleges across the US, paving the way for exceptional academic journeys."
         />
+      </div>
+
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-[90%]">
+          <InfoBox
+            title="2. What we offer"
+            content="Our team of experienced advisers will walk with you through every step, simplifying the process and ensuring your transition to studying abroad is exciting as it is seamless."
+            className="w-full"
+          />
+          <InfoBox
+            title="3. Our Commitment"
+            content="your success is our priority, we provide dependable hassle-free services designed to align perfectly with your goals and aspirations."
+            className="w-full"
+          />
+          <InfoBox
+            title="4. Our Mission"
+            content="Our mission is to help students fufill their academic and career goals by linking them with top-tier educational institutions in the US."
+            className="w-full"
+          />
+          <InfoBox
+            title="5. Our Vision?"
+            content="our vission is to become a global leader in the educational consultancy, crreating pathways for students to excel in diverse and thriving academic environments."
+            className="w-full"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/AboutusInfo.tsx
+++ src/components/AboutusInfo.tsx
@@ -6,8 +6,8 @@ type InfoBoxProps = {
   
   const InfoBox = ({ title, content, className = "" }: InfoBoxProps) => {
     return (
-      <div className={`bg-[#03577233] w-[80%] p-6 rounded-xl text-[#011D26] ${className}`}>
-        <h2 className="text-xl font-semibold mb-2">{title}</h2>
+      <div className={`bg-[#03577233] w-[80%] p-12 rounded-xl text-[#011D26] ${className}`}>
+        <h2 className="text-2xl font-semibold mb-5">{title}</h2>
         <p className="text-base leading-relaxed">{content}</p>
       </div>
     );

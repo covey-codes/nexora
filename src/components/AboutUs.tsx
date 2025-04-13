738805057c099aa7fddf069f625d8f85fa400b4e
covey-codes <dustindark7@gmail.com>
2025-04-13 14:19:22 +0100

    learnt somthing new about components

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,5 +1,20 @@
+import InfoBox from "@/components/AboutusInfo";
+
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      <div className="flex justify-center items-center m-10">
+        <h1 className="text-white bg-[#035772] p-5 rounded-[20px]">About Us</h1>
+      </div>
+
+      <div className="flex justify-center">
+        <InfoBox
+          title="1. Who are we?"
+          content="At Nexora, we are driven to help students achieve their dreams. By leveraging our expertise in student recruitment, we connect ambitious individuals with top universities and colleges across the US, paving the way for exceptional academic journeys."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
---
+++ src/components/AboutusInfo.tsx
@@ -0,0 +1,17 @@
+type InfoBoxProps = {
+    title: string;
+    content: string;
+    className?: string;
+  };
+  
+  const InfoBox = ({ title, content, className = "" }: InfoBoxProps) => {
+    return (
+      <div className={`bg-[#03577233] w-[80%] p-6 rounded-xl text-[#011D26] ${className}`}>
+        <h2 className="text-xl font-semibold mb-2">{title}</h2>
+        <p className="text-base leading-relaxed">{content}</p>
+      </div>
+    );
+  };
+  
+  export default InfoBox;
+  

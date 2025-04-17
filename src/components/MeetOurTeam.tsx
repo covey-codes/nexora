a1cf8238b548c153bc7e843d229c04b320da780f
covey-codes <dustindark7@gmail.com>
2025-04-17 16:13:22 +0100

    working on the team section

--- src/components/MeetOurTeam.tsx
+++ src/components/MeetOurTeam.tsx
@@ -2,7 +2,15 @@
 const MeetOurTeam = () => {
   return (
     <div>
-      
+      <div className="flex justify-center items-center flex-col mt-20">
+        <h1 className="text-2xl font-semi-bold mb-10">Meet Our Talented Team</h1>
+        <p>At the heart of our success is a team of passionate professionals driven by innovation <br /> and excellence. From creative thinkers to tech wizards, each member brings a unique <br /> expertise, collaborating seamlessly to achieve our vision.</p>
+        <div>
+          <button className="bg-[#011D26] text-white py-3 px-3 rounded-2xl mt-10">
+            Get In Touch
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
